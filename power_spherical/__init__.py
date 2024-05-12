e3d3e20380f4dff8f592530c6985bc07f517a289
Andreas Fehlner <fehlner@arcor.de>
2024-05-12 16:21:53 +0200

    update

--- power_spherical/__init__.py
+++ power_spherical/__init__.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2020 Nicola De Cao
+#
+# SPDX-License-Identifier: MIT
+
 from .distributions import HypersphericalUniform
 from .distributions import PowerSpherical
 from .distributions import MarginalTDistribution
